8c7a5926a0f010c99cd71f8f7a186700a3725676
pantosz <patryk.antosz@nokia.com>
2019-03-12 10:18:15 +0100

    Just little changes

--- factory.py
+++ factory.py
@@ -10,7 +10,8 @@ class Factory:
         print(reader.machines)
         if reader.machines == 2:
             for j in range(0, len(reader.machinesInFactory) - 1, 2):
-                productionLine = dataClass.TwoMachinesTask(reader.machinesInFactory[j], reader.machinesInFactory[j + 1])
+                productionLine = dataClass.TwoMachinesTask(reader.machinesInFactory[j],
+                                                           reader.machinesInFactory[j + 1])
                 self.twoMachines.append(productionLine)
         elif reader.machines == 3:
             for j in range(0, len(reader.machinesInFactory) - 1, 3):
@@ -20,11 +21,12 @@ class Factory:
                 self.threeMachines.append(productionLine)
 
     def print(self):
-        print("")
         if len(self.twoMachines) is not 0:
             for i in range(len(self.twoMachines)):
-                print(self.twoMachines[i].timeOnFirstMachine, self.twoMachines[i].timeOnSecondMachine)
+                print(self.twoMachines[i].timeOnFirstMachine,
+                      self.twoMachines[i].timeOnSecondMachine)
         else:
             for i in range(len(self.threeMachines)):
-                print(self.threeMachines[i].timeOnFirstMachine, self.threeMachines[i].timeOnSecondMachine,
+                print(self.threeMachines[i].timeOnFirstMachine,
+                      self.threeMachines[i].timeOnSecondMachine,
                       self.threeMachines[i].timeOnThirdMachine)
