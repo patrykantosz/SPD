ca3d18e2dc69fbd90a0f46310802f1e1ef471657
Patryk Antosz <papoj@wp.pl>
2019-04-26 21:20:35 +0200

    Added Schrage pmtn algorithm

--- Projekt_4/main.py
+++ Projekt_4/main.py
@@ -1,17 +1,37 @@
+from prettytable import PrettyTable as tabelki
+
+import makespan
 import read
 import schrage
-import makespan
 
 x = read.Reader()
-x.read("in50.txt")
+x.read("data//in50.txt")
 y = schrage.Schrage(x.my_data)
+y_1 = schrage.Schrage(x.my_data)
 
-tmp = []
+t = tabelki(['Alogrithm', 'in50', 'in100', 'in200', 'Sum'])
 
-x.read("in50.txt")
 sigma = y.do_schrage()
-for i in sigma:
-    tmp.append(i.id)
-print(tmp)
 c_max = makespan.makespan(sigma)
-print(c_max)
+c_max_pmtn = y_1.do_schrage_pmtn()
+
+x.read("data//in100.txt")
+y_2 = schrage.Schrage(x.my_data)
+y_3 = schrage.Schrage(x.my_data)
+
+sigma = y_2.do_schrage()
+c_max_1 = makespan.makespan(sigma)
+c_max_pmtn_1 = y_3.do_schrage_pmtn()
+
+x.read("data//in200.txt")
+y_4 = schrage.Schrage(x.my_data)
+y_5 = schrage.Schrage(x.my_data)
+
+sigma = y_4.do_schrage()
+c_max_2 = makespan.makespan(sigma)
+c_max_pmtn_2 = y_5.do_schrage_pmtn()
+
+t.add_row(['Schrage', c_max, c_max_1, c_max_2, c_max + c_max_1 + c_max_2])
+t.add_row(['Schrage pmtn', c_max_pmtn, c_max_pmtn_1, c_max_pmtn_2, c_max_pmtn + c_max_pmtn_1 + c_max_pmtn_2])
+
+print(t)
--- Projekt_4/schrage.py
+++ Projekt_4/schrage.py
@@ -1,4 +1,5 @@
 import task_class
+import math
 
 
 class Schrage:
@@ -6,8 +7,11 @@ class Schrage:
         self.sigma = []
         self.sorted_tasks = []
         self.unsorted_tasks = self.convert_tasks(tasks)
-        self.t = min(self.extract_r(self.convert_tasks(tasks)))
+        self.t = 0
         self.table_of_converted_tasks = self.convert_tasks(tasks)
+        self.l = 0
+        self.q = task_class.Tasks([0, 0, math.inf ],-1)
+        self.c_max = 0
 
     def extract_r(self, tasks):
         r_list = []
@@ -44,3 +48,35 @@ class Schrage:
                 self.sigma.append(self.sorted_tasks.pop(j))
                 self.t += self.sigma[-1].times[1]
         return self.sigma
+
+    def do_schrage_pmtn(self):
+        j = None
+        while self.sorted_tasks or self.unsorted_tasks:
+            while self.unsorted_tasks and (min(self.extract_r(self.unsorted_tasks)) <= self.t):
+                tmp_list = self.extract_r(self.unsorted_tasks)
+                j = tmp_list.index(min(tmp_list))
+                self.sorted_tasks.append(self.unsorted_tasks.pop(j))
+                if self.l == 0:
+                    if self.sorted_tasks[-1].times[2] > self.q.times[2]:
+                        self.l.times[1] = self.t - self.sorted_tasks[-1].times[0]
+                        self.t = self.sorted_tasks[-1].times[0]
+                        if self.l.times[1] > 0:
+                            self.sorted_tasks.append(self.l)
+                else:
+                    if self.sorted_tasks[-1].times[2] > self.l.times[2]:
+                        self.l.times[1] = self.t - self.sorted_tasks[-1].times[0]
+                        self.t = self.sorted_tasks[-1].times[0]
+                        if self.l.times[1] > 0:
+                            self.sorted_tasks.append(self.l)
+
+            if not self.sorted_tasks:
+                self.t = min(self.extract_r(self.unsorted_tasks))
+            else:
+                qs = self.extract_q(self.sorted_tasks)
+                j = qs.index(max(qs))
+                self.l = self.sorted_tasks.pop(j)
+                #print("L wynosi: ", self.l.id)
+                self.t += self.l.times[1]
+                self.c_max = max(self.c_max, self.t + self.l.times[2])
+        return self.c_max
+
