c225b8bffe37a258ace096bb57661467260f4374
Patryk Antosz <papoj@wp.pl>
2019-05-22 21:45:46 +0200

    Finished Carlier algorith, works for 8/12 tests

--- Projekt_5/carlier.py
+++ Projekt_5/carlier.py
@@ -1,7 +1,26 @@
-import math
+import copy
 
 import makespan
 import schrage
+
+
+def calc_k(carlier_object, K_):
+    # r_(K), q_(K), p_(K)
+    potential_r_k_ = []
+    potential_p_k_ = []
+    potential_q_k_ = []
+    for j in K_:
+        potential_r_k_.append(carlier_object.pi[j].times[0])
+        potential_p_k_.append(carlier_object.pi[j].times[1])
+        potential_q_k_.append(carlier_object.pi[j].times[2])
+    r_k_ = min(potential_r_k_)
+    p_k_ = sum(potential_p_k_)
+    q_k_ = min(potential_q_k_)
+
+    # h(K u {c})
+    h_k_ = r_k_ + p_k_ + q_k_
+
+    return h_k_, r_k_, p_k_, q_k_
 
 
 def find_by_id(carlier_object, id):
@@ -13,43 +32,89 @@ def find_by_id(carlier_object, id):
             index += 1
     return -1
 
+
 def do_carlier(carlier_object):
     carlier_object.U, carlier_object.pi = schrage.do_schrage(carlier_object.tasks)
-    for i in carlier_object.pi:
-        print("ID: ", i.id)
 
     if carlier_object.U < carlier_object.UB:
-        carlier_object.UB = carlier_object.U
-        carlier_object.opt_pi = carlier_object.pi
+        carlier_object.UB = copy.deepcopy(carlier_object.U)
+        carlier_object.opt_pi = copy.deepcopy(carlier_object.pi)
 
-    #b index
+    # b index
     potential_b = []
     for j in range(0, len(carlier_object.tasks)):
-        if makespan.makespan(carlier_object.pi) == (carlier_object.pi[j].times[2] + makespan.makespan_carlier(carlier_object.pi)[j]):
+        if makespan.makespan(carlier_object.pi) == (
+                carlier_object.pi[j].times[2] + makespan.makespan_carlier(carlier_object.pi)[j]):
             potential_b.append(carlier_object.pi[j].id)
     carlier_object.b = max(potential_b)
-    print("B: ", carlier_object.b)
 
-    #a index
-    potential_a = []
-    for j in range(0, carlier_object.b + 1):
+    # a index
+    for j in range(0, len(carlier_object.tasks)):
         sum_of = 0
-        for i in range(j, carlier_object.b):
+        for i in range(j, find_by_id(carlier_object, carlier_object.b) + 1):
             sum_of += carlier_object.pi[i].times[1]
-        if makespan.makespan(carlier_object.pi) == (sum_of + carlier_object.pi[j].times[0] + carlier_object.tasks[carlier_object.b].times[2]):
-            potential_a.append(carlier_object.pi[j].id)
-    carlier_object.a = min(potential_a)
-    print("A: ", carlier_object.a)
+        if carlier_object.U == (
+                sum_of + carlier_object.pi[j].times[0] + carlier_object.tasks[carlier_object.b].times[2]):
+            carlier_object.a = carlier_object.pi[j].id
+            break
+    carlier_object.a = find_by_id(carlier_object, carlier_object.a)
+    carlier_object.b = find_by_id(carlier_object, carlier_object.b)
 
-    #c index
+    # c index
     potential_c = []
-    index_a_in_pi = find_by_id(carlier_object, carlier_object.a)
-    index_b_in_pi = find_by_id(carlier_object, carlier_object.b)
-    for j in range(index_a_in_pi, index_b_in_pi + 1):
+    for j in range(carlier_object.a, carlier_object.b + 1):
         if carlier_object.pi[j].times[2] < carlier_object.pi[carlier_object.b].times[2]:
             potential_c.append(j)
-    print(max(potential_c))
+    if potential_c:
+        carlier_object.c = max(potential_c)
+    else:
+        return carlier_object.U
 
+    # K block
+    K = []  # K to blok, w którym są indeksy do pi[index]
+    for j in range(carlier_object.c + 1, carlier_object.b + 1):
+        K.append(j)
 
+    # K u {c} block
+    K_ = []  # K to blok, w którym są indeksy do pi[index]
+    for j in range(carlier_object.c, carlier_object.b + 1):
+        K_.append(j)
 
+    # r(K), q(K), p(K), h(K)
+    h_k, r_k, p_k, q_k = calc_k(carlier_object, K)
 
+    # remember tasks[c].times[0]
+    old_r_pi = copy.deepcopy(carlier_object.tasks[carlier_object.pi[carlier_object.c].id].times[0])
+
+    # new tasks[c].times[0]
+    carlier_object.tasks[carlier_object.pi[carlier_object.c].id].times[0] = max(
+        carlier_object.tasks[carlier_object.pi[carlier_object.c].id].times[0], r_k + p_k)
+    carlier_object.LB = schrage.do_schrage_pmtn(carlier_object.tasks)[0]
+
+    h_k_c, r_k_c, p_k_c, q_k_c = calc_k(carlier_object, K_)
+    carlier_object.LB = max(h_k, h_k_c, carlier_object.LB)
+
+    # left if
+    if carlier_object.LB < carlier_object.UB:
+        do_carlier(carlier_object)
+
+    # restore tasks[c].times[0]
+    carlier_object.tasks[carlier_object.pi[carlier_object.c].id].times[0] = old_r_pi
+
+    # remember tasks[c].times[2]
+    old_q_pi = copy.deepcopy(carlier_object.pi[carlier_object.c].times[2])
+
+    carlier_object.tasks[carlier_object.pi[carlier_object.c].id].times[2] = max(
+        carlier_object.tasks[carlier_object.pi[carlier_object.c].id].times[2],
+        q_k + p_k)  # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+    carlier_object.LB = schrage.do_schrage_pmtn(carlier_object.tasks)[0]
+
+    h_k_c, r_k_c, p_k_c, q_k_c = calc_k(carlier_object, K_)
+    carlier_object.LB = max(h_k, h_k_c, carlier_object.LB)
+
+    if carlier_object.LB < carlier_object.UB:
+        do_carlier(carlier_object)
+    carlier_object.tasks[carlier_object.pi[carlier_object.c].id].times[2] = old_q_pi
+
+    return carlier_object.U
--- Projekt_5/main.py
+++ Projekt_5/main.py
@@ -1,20 +1,34 @@
-from prettytable import PrettyTable as tabelki
+import glob
+import os
 
-import carlier_task
-import makespan
-import read
-import schrage
-import converter
+from prettytable import PrettyTable as tabel
+
 import carlier
+import carlier_task
+import converter
+import read
 
+# load files
+files = list(glob.glob(os.path.join("data", '*')))
+
+correct_times = [228, 3026, 3665, 3309, 3191, 3618, 3446, 3821, 3634, 3070, 6398, 1492]
+t = tabel(['File name', 'Correct time', 'Calculated time', 'Is calculated time correct?'])
+index = 0
+correct = 0
 x = read.Reader()
-x.read("data//in50.txt")
-converted_tasks = converter.converter(x.my_data)
-carlier_object = carlier_task.Carlier_Task(converted_tasks)
 
+for file in files:
+    x.read(file)
+    converted_tasks = converter.converter(x.my_data)
+    carlier_object = carlier_task.Carlier_Task(converted_tasks)
+    c_max = carlier.do_carlier(carlier_object)
+    t.add_row([file[5:], correct_times[index], c_max, correct_times[index] == c_max])
+    if correct_times[index] == c_max:
+        correct += 1
+    index += 1
+string = str(str(correct) + "/" + str(index))
+print(string)
+t.add_row(["", "", "", ""])
+t.add_row(["", "", "Passed: ", string])
 
-c_max, list = schrage.do_schrage(converted_tasks)
-print(c_max)
-c_max, list = schrage.do_schrage_pmtn(converted_tasks)
-print(c_max)
-carlier.do_carlier(carlier_object)
+print(t)
